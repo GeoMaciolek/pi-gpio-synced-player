4440535841886de63fa06562904321d00b14404e
Geo Maciolek <GeoffMaciolek@gmail.com>
2024-01-10 19:28:35 -0500

    Lots of changes for vlc playback!

--- pi-gpio-synced-player.py
+++ pi-gpio-synced-player.py
@@ -1,7 +1,9 @@
-ver = 'pi-gpio-synced-player.py 0.5 - mpv edition'
+ver = 'pi-gpio-synced-player.py 0.6 - vlc edition'
 
 import time
-import mpv
+import vlc
+
+from datetime import datetime
 
 # Set to True to skip GPIO etc - for testing NOT on a pi
 TEST_MODE = False
@@ -41,21 +43,19 @@ if not TEST_MODE:
 ### End Application Settings ###
 ################################
 
-def vid_pauseplay():
-    print('send_key(" ")')
+def timestamp():
+    return datetime.now().strftime("%H:%M:%S.%f")
 
-def vid_rewind():
-    print('send_key("i")')
-
-def vid_quit():
-    print('send_key("q")')
+def vid_quit(vlc_player):
+    #print('send_key("q")')
+    vlc_player.quit()
 
 # Reset to start - assumes video is playing, and leaves it paused after
 def wait_for_gpio():
     if TEST_MODE:
         print('[TEST MODE] Waiting for a rising pin (just sleep 5).')
         time.sleep(5)
-        print('We got a rising pin!')
+        print(f'{timestamp()} - We got (fake) rising pin!')
     else:
         print('Waiting for a rising pin.')
         GPIO.wait_for_edge(gpio_listen_pin, GPIO.RISING)
@@ -87,14 +87,14 @@ def gpio_send_pin_high():
         for gpio_transmit_pin in gpio_transmit_pins:
             GPIO.output(gpio_transmit_pin, GPIO.HIGH)
     else:
-        print("[TEST MODE] We would be setting the GPIO pins high.")
+        print(f"{timestamp()} - [TEST MODE] We would be setting the GPIO pins high.")
 
 def gpio_send_pin_low():
     if not TEST_MODE:
         for gpio_transmit_pin in gpio_transmit_pins:
             GPIO.output(gpio_transmit_pin, GPIO.LOW)
     else:
-        print("[TEST MODE] We would be setting the GPIO pins low.")
+        print(f"{timestamp()} - [TEST MODE] We would be setting the GPIO pins low.")
 
 def setup_gpio_listen_pin():
     if not TEST_MODE:
@@ -103,60 +103,98 @@ def setup_gpio_listen_pin():
     else:
         print("[TEST MODE] We would be setting up the GPIO listen pin.")
 
-def player_launch():
+def player_launch(media_file:str, set_playback_count=1) -> (vlc.MediaPlayer, vlc.Instance, vlc.Media):
     """launch the media player
 
+    Args:
+        media_file (str): The file to play.
+        set_playback_count (int, optional): The number of times to play the media. Defaults to 1.
+
     Returns:
-        mpv.Context: the player context
+        (vlc.MediaPlayer, vlc.Instance, vlc.Media, duration [int]): a tuple of the player, instance, and media
     """
     try:
-        player = mpv.Context()
-    except mpv.MPVError:
+
+        # creating a vlc instance - with a LONG repeat number
+        vlc_instance = vlc.Instance(f'--input-repeat={set_playback_count}')
+     
+        # creating a media player
+        vlc_player = vlc_instance.media_player_new()
+
+        # load the media
+        vlc_media = player_open_file(instance=vlc_instance, player=vlc_player, input_file=media_file)
+
+    except vlc.VLCException as e:
         print('failed creating context')
+        print(e)
         return 1
     
-    player.set_option('input-default-bindings')
-    if osd_enabled:
-        player.set_option('osc')
-    player.set_option('input-vo-keyboard')
-    player.set_option('loop', 'yes')
-    player.set_option('pause') # start playback paused
-    player.initialize()
+    vlc_player.set_fullscreen(1)
 
-    return player
+    # Play a little bit to get the duratioN!    
+    vlc_player.play()
+    time.sleep(0.1)
+    vlc_player.pause()
 
-def player_open_file(player, input_file):
-    print(f'player.play({input_file})')
-    player.command('loadfile', input_file)
+    media_duration = vlc_media.get_duration()
 
-def player_reset_to_start(player):
+    
+    # if osd_enabled:
+    #     player.set_option('osc')
+
+    return vlc_player, vlc_instance, vlc_media, media_duration
+
+def player_open_file(instance, player, input_file) -> vlc.Media:
+    """_summary_
+
+    Args:
+        instance (vlc.Instance): the instance of the VLC module
+        player (vlc.MediaPlayer): the player instance itself
+        input_file (string): the filepath/filename of the file to play
+
+    Returns:
+        vlc.Media: media file (instance)
+    """
+    print(f'player_open_file([player], {input_file})')
+
+    media = instance.media_new(input_file)
+
+    player.set_media(media)
+
+    return media
+
+def player_reset_to_start(player: vlc.MediaPlayer):
     print('Resetting to start')
-    player.set_option('pause', 'yes')
-    player.command('seek','0','absolute')
+    player.set_time(0)
+    print('time reset.')
 
 def player_resume(player):
     print('Resuming playback')
-    player.set_option('pause', 'no')
+    player.play()
 
 def player_start_at_beginning(player):
     print("Starting playback at beginning: player_reset_to_start(), then player_resume()")
     player_reset_to_start(player=player)
     player_resume(player=player)
 
-def player_wait_for_end(player):
-    print('Waiting...')
-    while player.get_property('time-pos') < (player.get_property('duration')-0.3):
-        time.sleep(0.1)
-        # print(player.get_property('time-pos'))
+def player_wait_for_end(player: vlc.MediaPlayer, duration: int):
+    """
+    This function waits until the media player is near the end of the media file.
 
-    # while True:
-        # event = player.wait_event(.01)
-        # if event.id == mpv.Events.none:
-        #     continue
-        # print(f"mpv event: {event.name}")
-        # if event.id in [mpv.Events.end_file, mpv.Events.shutdown]:
-        #     return True
+    Args:
+        player (vlc.MediaPlayer): The media player instance.
+        duration (int): The total duration of the media file in milliseconds.
 
+    """
+
+    # checks current time/pos of media player, compares  w/the specified duration
+    # If current time is less than the duration minus a small buffer (three times the wait state), 
+    # pause execution for a short period (defined by wait_state_ms) and check again.
+    # print('Waiting...')
+    wait_state_ms = 100
+    near_the_end = duration - (wait_state_ms*3)
+    while player.get_time() < near_the_end:
+        time.sleep(wait_state_ms // 1000)
 
 # Intialize pi GPIO
 gpio_initialize()
@@ -164,6 +202,28 @@ gpio_initialize()
 print(f'{ver} in mode: {mode}')
 
 count = 0
+
+if play_forever:
+    set_playback_count = 65535
+else:
+    set_playback_count = playback_count
+
+print('Running player init')
+# Initialize player (regardless of primary or secondary mode)
+player, instance, media, duration = player_launch(media_file=media_file, set_playback_count=set_playback_count)
+
+print('Player init should be done')
+
+# Get file length in ms
+# duration = media.get_duration()
+
+print(f'{duration=}')
+print(player)
+print(instance)
+
+print('sleeping after init')
+time.sleep(3)
+print('continuing')
 
 if mode == 'primary':
 
@@ -174,13 +234,9 @@ if mode == 'primary':
 
 #    # Set Pin 2 ito High
 #    GPIO.output(gpio_transmit_pin, GPIO.HIGH)
-#
-    # Load file
-    player = player_launch()
-    player_open_file(player=player, input_file=media_file)
 
     # Wait for file to load / buffer
-    time.sleep(load_wait_duration)      # [5] sec
+    time.sleep(load_wait_duration // 1000)      # [5] sec
     
     # Begin standard playback loop
     while (count <= playback_count) or play_forever:
@@ -188,14 +244,16 @@ if mode == 'primary':
         # Pins to high - 2nd trigger
         gpio_send_pin_high()
 
+        print(f'{timestamp()} - launch: player_start_at_beginning()')
         player_start_at_beginning(player=player)
-
-        # Pins to low (will include the [1 sec] delay of keypress
+        print(f'{timestamp()} - end:    player_start_at_beginning()')
+        # Pins to low
+        time.sleep(0.5)
         gpio_send_pin_low()
 
-        print('Waiting for the end of the video...')
-        player_wait_for_end(player=player)
-        print('Video has ended!')
+        print(f'{timestamp()} - Waiting for the end of the video...')
+        player_wait_for_end(player=player, duration=duration)
+        print(f'{timestamp()} - Video has ended!')
 
         count += 1
 
@@ -207,19 +265,17 @@ if mode == 'primary':
 elif mode == 'secondary':
     # Set up listening pin
     setup_gpio_listen_pin()
-    # Initialize (load, reset to start)
-    player = player_launch()
-    player_open_file(player=player, input_file=media_file)
 
     # Wait for file to load / buffer
     time.sleep(load_wait_duration - 1)      # [5 - 1] sec
 
 
-    print('++++ Prepared to start ++++')
+    print('Prepared to start (as secondary)')
 
     # Begin standard playback loop
     while (count <= playback_count) or play_forever:
         # Wait for RISE (GPIO going High on Main)
+
         wait_for_gpio()
         player_start_at_beginning(player=player)
 
