8b222a5a59213787b3ed9fbbcb305461442f0836
Geo Maciolek <GeoffMaciolek@gmail.com>
2024-01-15 01:43:46 -0500

    support wayfire as well as lxsession for autostart. generate wrapper file dyanmically.

--- lxterm-wrapper.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-lxterminal -l -e '~/Downloads/pi-gpio-synced-player/playback-wrapper.sh; bash'
--- setup-autostart.sh
+++ setup-autostart.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-autostartFile="/etc/xdg/lxsession/LXDE-pi/autostart"
 autostartScript="lxterm-wrapper.sh"
+# OS Specific files
+lxdeAutostartFile="/etc/xdg/lxsession/LXDE-pi/autostart"
+
+wayfireAutostartFile="$HOME/.config/wayfire.ini"
+# Player name used in wayfire.ini
+playerName="wavesplayer"
 
 ### Init some variables
+
 autostartSciptLocation="$PWD/$autostartScript"
+
+
+# Identify suppurted Raspbian versions
+case $(cat /etc/debian_version) in
+11) VERSION=11;;
+12) VERSION=12;;
+*) VERSION=OTHER;;
+esac
+
+if [ "$VERSION" = "OTHER" ]; then
+    echo "This script is only for Raspbian 11 (Bullseye) and 12 (Bookworm). Exiting."
+    exit 1
+fi
 
 # exit if not running as root
 if [[ $EUID -ne 0 ]]; then
@@ -12,30 +31,93 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Check if autostart script exists in the expected location
-if [ ! -f "$autostartSciptLocation" ]; then
-    echo "No autostart script where expected: $autostartSciptLocation. Exiting."
+# Check if autostart script (to be installed) exists in the expected location
+if [ -f "$autostartSciptLocation" ]; then
+    echo "Existing autostart script where none expected: $autostartSciptLocation. Exiting."
     exit 1
 fi
 
-# Check if autostart file contains the player command
-if grep -q "$autostartScript" "$autostartFile"; then
-    echo "Autostart file already contains the player command. Exiting."
-    exit 1
+#### Create lxterm wrapper ####
+wrapperStart="lxterminal -l -e '"
+wrapperEnd="; bash'"
+
+wrapperCommand="$wrapperStart$autostartSciptLocation$wrapperEnd"
+echo "Creating wrapper script: $autostartSciptLocation"
+echo "      $wrapperCommand"
+echo "$wrapperCommand" > "$autostartSciptLocation"
+
+
+############################################################
+### Raspberry Pi OS 11 (Bullseye) version (LXDE / lxsession)
+
+if [ "$VERSION" = "11" ]; then
+    echo "Installing autostart for Raspberry Pi OS 11 (Bullseye) (LXDE / lxsession)"
+
+    # Check if lxde's autostart file exists
+    if [ ! -f "$lxdeAutostartFile" ]; then
+        echo "No LXDE autostart file where expected: $lxdeAutostartFile. Exiting."
+        exit 1
+    fi
+
+
+    # Check if autostart file contains the player command
+    if grep -q "$autostartScript" "$lxdeAutostartFile"; then
+        echo "Autostart file already contains the player command. Exiting."
+        exit 1
+    fi
+
+    # Copy the player autostart command file to the autostart file
+    echo "Adding player autostart command to autostart file."
+
+    autostartLine="@$autostartSciptLocation"
+
+    # Note - adds a newline at the end. This is intentional.
+    echo "$autostartLine" >> "$lxdeAutostartFile"
+
+    # Check if the player autostart command was added to the autostart file
+    if grep -q "$autostartScript" "$lxdeAutostartFile"; then
+        echo "Player autostart command added to autostart file."
+    else
+        echo "Error: Player autostart command not added to autostart file. Exiting."
+        exit 1
+    fi
+    echo "Installation of autostart complete"
+    exit 0
 fi
 
-# Copy the player autostart command file to the autostart file
-echo "Adding player autostart command to autostart file."
+###################################################
+### Raspberry Pi OS 12 (Bookworm) version (wayfire)
 
-autostartLine="@$autostartSciptLocation"
+if [ "$VERSION" = "12" ]; then
 
-# Note - adds a newline at the end. This is intentional.
-echo "$autostartLine" >> "$autostartFile"
+    echo "Installing autostart for Raspberry Pi OS 12 (Bookworm) (wayfire)"
 
-# Check if the player autostart command was added to the autostart file
-if grep -q "$autostartScript" "$autostartFile"; then
-    echo "Player autostart command added to autostart file."
-else
-    echo "Error: Player autostart command not added to autostart file. Exiting."
-    exit 1
+    # Check for wayfire autostart file
+    if [ ! -f "$wayfireAutostartFile" ]; then
+        echo "No wayfire autostart file where expected: $wayfireAutostartFile. Exiting."
+        exit 1
+    fi
+
+    # If wayfire.ini file already has an our entry, exit
+    if grep -q "$playerName" "$wayfireAutostartFile"; then
+        echo "wayfire.ini file already has an entry for $playerName. Exiting."
+        exit 1
+    fi
+
+    wayfireLauncherLine="$playerName = $autostartSciptLocation"
+
+    # if wayfire.ini file already has an autostart section, insert line below it
+    if grep -q "\[autostart\]" "$wayfireAutostartFile"; then
+        echo "Adding line to existing [autpstart] section of $wayfireAutostartFile"
+        sed -i "/\[autostart\]/a $wayfireLauncherLine" "$wayfireAutostartFile"
+        exit 0
+    else
+        echo "Adding [autostart] section to $wayfireAutostartFile"
+        echo "[autostart]" >> "$wayfireAutostartFile"
+        echo "$wayfireLauncherLine" >> "$wayfireAutostartFile"
+        exit 0
+    fi
 fi
+
+echo "The script shouldn't  get here, so, if you see this, something went wrong. Exiting."
+exit 1
