dbe126b369096fad8bea7464599c5530fc3a9f58
Geo Maciolek <GeoffMaciolek@gmail.com>
2024-01-15 00:02:25 -0500

    Woah, that was way more complex than it needed to be.

--- setup-autostart.sh
+++ setup-autostart.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 autostartFile="/etc/xdg/lxsession/LXDE-pi/autostart"
-playerAutostartCommandFile=lxde-autostart
+autostartScript="lxterm-wrapper.sh"
 
-playerAutostartCommandFileTemplate="$playerAutostartCommandFile-template"
+### Init some variables
+autostartSciptLocation="$PWD/$autostartScript"
 
 # exit if not running as root
 if [[ $EUID -ne 0 ]]; then
@@ -11,54 +12,28 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Check if autostart file exists
-if [ ! -f "$autostartFile" ]; then
-    echo "Autostart file not found. Exiting."
+# Check if autostart script exists in the expected location
+if [ ! -f "$autostartSciptLocation" ]; then
+    echo "No autostart script where expected: $autostartSciptLocation. Exiting."
     exit 1
 fi
 
 # Check if autostart file contains the player command
-if grep -q "pi-gpio-synced-player" "$autostartFile"; then
+if grep -q "$autostartScript" "$autostartFile"; then
     echo "Autostart file already contains the player command. Exiting."
     exit 1
 fi
 
-# Check if the player autostart command file exists
-if [ ! -f "$playerAutostartCommandFileTemplate" ]; then
-    echo "Player autostart command file not found. Exiting."
-    exit 1
-fi
-
-# if originalUser is root, exit
-if [ "$SUDO_USER" = "root" ]; then
-    echo "You didn't use sudo, but were running as root. I'm confused. Exiting."
-    exit 1
-fi
-
-# Find original user's home folder
-originalUserHome=$(eval echo "~$SUDO_USER")
-
-
-# Replace the tilde in the player autostart command file with the home directory
-d=$'\03'
-sed "s${d}\~${d}$originalUserHome$d" < "$playerAutostartCommandFileTemplate" > "$playerAutostartCommandFile"
-
-
-# Check if the modified player autostart command file exists
-if [ ! -f "$playerAutostartCommandFile" ]; then
-    echo "(Generated) player autostart command file not found. Exiting."
-    exit 1
-fi
-
-
 # Copy the player autostart command file to the autostart file
 echo "Adding player autostart command to autostart file."
-cat "$playerAutostartCommandFile" >> "$autostartFile"
 
+autostartLine="@$autostartSciptLocation"
 
+# Note - adds a newline at the end. This is intentional.
+echo "$autostartLine" >> "$autostartFile"
 
 # Check if the player autostart command was added to the autostart file
-if grep -q "pi-gpio-synced-player" "$autostartFile"; then
+if grep -q "$autostartScript" "$autostartFile"; then
     echo "Player autostart command added to autostart file."
 else
     echo "Error: Player autostart command not added to autostart file. Exiting."
